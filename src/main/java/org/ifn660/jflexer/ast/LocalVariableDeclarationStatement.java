122903266990388a07768c982fc5eed0e3e65ce2
marshall <zyx954@gmail.com>
2016-04-23 18:03:02 +1000

    change ASTDriver to another style tree stucture

--- src/main/java/org/ifn660/jflexer/ast/LocalVariableDeclarationStatement.java
+++ src/main/java/org/ifn660/jflexer/ast/LocalVariableDeclarationStatement.java
@@ -1,12 +1,10 @@
 package org.ifn660.jflexer.ast;
 
 public class LocalVariableDeclarationStatement extends Node {
-    private VariableDeclaratorId varDecId;
     private VariableInitializer varInit;
     private Type type;
     
-    public LocalVariableDeclarationStatement(Type type, VariableDeclaratorId varDecId, VariableInitializer varInit) {
-        this.varDecId = varDecId;
+    public LocalVariableDeclarationStatement(Type type,  VariableInitializer varInit) {
         this.varInit = varInit;
         this.type = type;
     }
