1cd1c22c7bb055f4b0ab35967098bab58c4735c5
Morteza Bokharaie <morteza.bokharaie@gmail.com>
2016-05-31 11:42:05 +1000

    Complete the code for X * Y

--- resources/Example.java
+++ resources/Example.java
@@ -5,7 +5,13 @@ public class Example {
     public static void main(String[] args) {
         int x = 42;
         int y;
+        int z;
+
         y = x + 31;
+        z = x * y;
+ 
+        System.out1.println(z);
         System.out1.println(y);
+       
     }
 }
